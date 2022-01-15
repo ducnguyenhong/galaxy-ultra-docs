ec0c0c2dc8e91be58d19d5f255095268e8eab88c
Nguyễn Đức <autoclickvn@gmail.com>
2022-01-16 01:33:54 +0700

    Update Modal UI

--- pages/components/modal/modal.tsx
+++ pages/components/modal/modal.tsx
@@ -10,6 +10,12 @@ import { useState } from 'react';
 
 const ModalUI: React.FC = () => {
   const [showModal, setShowModal] = useState<boolean>(false);
+  const [showModalSizeSM, setShowModalSizeSM] = useState<boolean>(false);
+  const [showModalSizeMD, setShowModalSizeMD] = useState<boolean>(false);
+  const [showModalSizeLG, setShowModalSizeLG] = useState<boolean>(false);
+  const [showModalSizeXL, setShowModalSizeXL] = useState<boolean>(false);
+  const [showModalPositionTop, setShowModalPositionTop] = useState<boolean>(false);
+  const [showModalPositionCenter, setShowModalPositionCenter] = useState<boolean>(false);
 
   return (
     <div className='grid grid-cols-4 gap-x-20 w-full'>
@@ -44,6 +50,115 @@ const ModalUI: React.FC = () => {
             </div>
           </div>
           <ShowCode code={MODAL_CODE_DEMO_NORMAL} link={MODAL_CODE_SANDBOX_NORMAL} />
+        </div>
+
+        {/* Modal Size */}
+        <div className='mt-20'>
+          <h3 className='font-semibold mb-5 text-3xl dark:text-gray-300' id="anchor-modal-size">Modal size</h3>
+          <div className='flex items-center border dark:border-gray-600 rounded px-5 py-10 flex-col'>
+            <div>
+              <div className='grid grid-cols-4 gap-x-10'>
+                <div className="col-span-1">
+                  <Button onClick={() => setShowModalSizeSM(true)} variant="primary">
+                    Size SM
+                  </Button>
+                </div>
+                <div className="col-span-1">
+                  <Button onClick={() => setShowModalSizeMD(true)} variant="success">
+                    Size MD
+                  </Button>
+                </div>
+                <div className="col-span-1">
+                  <Button onClick={() => setShowModalSizeLG(true)} variant="danger">
+                    Size LG
+                  </Button>
+                </div>
+                <div className="col-span-1">
+                  <Button onClick={() => setShowModalSizeXL(true)} variant="dark">
+                    Size XL
+                  </Button>
+                </div>
+              </div>
+              <div>
+                <Modal show={showModalSizeSM} onClose={() => setShowModalSizeSM(false)} size="sm">
+                  <Modal.Header>Modal Title</Modal.Header>
+                  <Modal.Body>
+                    <div className='text-justify'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</div>
+                  </Modal.Body>
+                  <Modal.Footer />
+                </Modal>
+
+                <Modal show={showModalSizeMD} onClose={() => setShowModalSizeMD(false)} size="md">
+                  <Modal.Header>Modal Title</Modal.Header>
+                  <Modal.Body>
+                    <div className='text-justify'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</div>
+                  </Modal.Body>
+                  <Modal.Footer />
+                </Modal>
+
+                <Modal show={showModalSizeLG} onClose={() => setShowModalSizeLG(false)} size="lg">
+                  <Modal.Header>Modal Title</Modal.Header>
+                  <Modal.Body>
+                    <div className='text-justify'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</div>
+                  </Modal.Body>
+                  <Modal.Footer />
+                </Modal>
+
+                <Modal show={showModalSizeXL} onClose={() => setShowModalSizeXL(false)} size="xl">
+                  <Modal.Header>Modal Title</Modal.Header>
+                  <Modal.Body>
+                    <div className='text-justify'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</div>
+                  </Modal.Body>
+                  <Modal.Footer />
+                </Modal>
+              </div>
+            </div>
+          </div>
+          <div className='flex justify-end mt-5'>
+            <span>See full <span className='text-teal-500 font-medium'>size</span> at <span className='font-semibold'>Props</span></span>
+          </div>
+        </div>
+
+        {/* Modal Position*/}
+        <div className='mt-20'>
+          <h3 className='font-semibold mb-5 text-3xl dark:text-gray-300' id="anchor-modal-position">Modal position</h3>
+          <div className='flex items-center border dark:border-gray-600 rounded px-5 py-10 flex-col'>
+            <div>
+              <div className='grid grid-cols-2 gap-x-10'>
+                <div className="col-span-1">
+                  <Button onClick={() => setShowModalPositionTop(true)} variant="success">
+                    Top modal
+                  </Button>
+                </div>
+                <div className="col-span-1">
+                  <Button onClick={() => setShowModalPositionCenter(true)} variant="danger">
+                    Center modal
+                  </Button>
+                </div>
+              </div>
+
+              <div>
+                <Modal show={showModalPositionTop} onClose={() => setShowModalPositionTop(false)} position="top">
+                  <Modal.Header>Modal Title</Modal.Header>
+                  <Modal.Body>
+                    <div className='text-justify'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</div>
+                  </Modal.Body>
+                  <Modal.Footer />
+                </Modal>
+
+                <Modal show={showModalPositionCenter} onClose={() => setShowModalPositionCenter(false)} position="center">
+                  <Modal.Header>Modal Title</Modal.Header>
+                  <Modal.Body>
+                    <div className='text-justify'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</div>
+                  </Modal.Body>
+                  <Modal.Footer />
+                </Modal>
+              </div>
+            </div>
+          </div>
+          <div className='flex justify-end mt-5'>
+            <span>See full <span className='text-teal-500 font-medium'>position</span> at <span className='font-semibold'>Props</span></span>
+          </div>
         </div>
 
         {/* PROPS Modal*/}
@@ -87,6 +202,8 @@ const ModalUI: React.FC = () => {
         <div className="sticky right-0 top-32">
           <DocsAnchor items={[
             { id: 'anchor-modal-normal', title: 'Modal' },
+            { id: 'anchor-modal-size', title: 'Modal size' },
+            { id: 'anchor-modal-position', title: 'Modal position' },
             { id: 'anchor-modal-props', title: 'Props' },
             { id: 'anchor-modal-props-modal', title: 'Modal', secondary: true },
             { id: 'anchor-modal-props-header', title: 'Modal.Header', secondary: true },
